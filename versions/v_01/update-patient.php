2de93c33f6dd00c9a4c3f926a6f34e649066de1a
Tanjir Ahmed Nadim <106719777+tan45Nadim@users.noreply.github.com>
2023-04-25 23:22:01 +0600

    Add files via upload

---
+++ versions/v_01/update-patient.php
@@ -0,0 +1,87 @@
+<?php
+    session_start();
+    require 'db-connect.php'
+?>
+
+<?php
+    include('includes/header.php');
+?>
+
+<div class="container mt-5">
+
+    <?php include('message.php'); ?>
+
+    <div class="row">
+        <div class="col-md12">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Update Patient
+                        <a href="search-patient.php" class = "btn btn-danger float-end">Back</a>
+                    </h4>
+                </div>
+                <div class="card-body">
+
+                <?php
+                    if (isset($_GET['p_id'])) {
+                        $p_id = mysqli_real_escape_string($connect, $_GET['p_id']);
+                        $query = "SELECT * FROM patient where p_id = '$p_id' ";
+                        $query_run = mysqli_query($connect, $query);
+
+                        if (mysqli_num_rows($query_run) > 0) {
+                            $p_arr = mysqli_fetch_array($query_run);
+                            
+                            ?>
+                            <form action="source-codes.php" method="POST">
+                                <!-- taking id -->
+                                <!-- <input type="hidden" name = "p_id" value="<?= $p_id ?>"> -->
+                                <input type="hidden" name = "p_id" value="<?= $p_arr['p_id'] ?>">                                    
+
+                                <div class="mb-3">
+                                    <label>Patient Name</label>
+                                    <input type="text" name = "p_name" class="form-control" value="<?= $p_arr['p_name'] ?>" required>
+                                </div>
+                                <div class="mb-3">
+                                    <label>Patient Age</label>
+                                    <input type="number" name = "p_age" class="form-control" value="<?= $p_arr['p_age'] ?>" required>
+                                </div>
+                                <div class="mb-3">
+                                    <label>Patient Sex</label>
+                                    <input type="text" name = "p_sex" class="form-control" value="<?= $p_arr['p_sex'] ?>" required>
+                                </div>
+                                <div class="mb-3">
+                                    <label>Patient Mobile Number</label>
+                                    <input type="number" name = "mobile_no" class="form-control" value="<?= $p_arr['mobile_no'] ?>" required>
+                                </div>
+                                <div class="mb-3">
+                                    <label>Patient Address</label>
+                                    <input type="text" name = "address" class="form-control" value="<?= $p_arr['address'] ?>" required>
+                                </div>
+                                <!-- <div class="mb-3">
+                                    <label>Patient's Residential Status</label>
+                                    <input type="text" name = "isResident" class="form-control" value="<?= $p_arr['isResident'] ?>" required>
+                                </div> -->
+                                <div class="mb-3">
+                                    <label>Patient Room Number</label>
+                                    <input type="text" name = "room_no" class="form-control" value="<?= $p_arr['room_no'] ?>">
+                                </div> 
+                                <div class="mb-3">
+                                    <button type="submit" name="update_patient" class="btn btn-primary">Update Patient</button>
+                                </div>
+                            </form>                                
+                            <?php
+
+                        } else {
+                            echo "<h4>No Such Patient Found!</h4>";
+                        }
+                        
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+    include('includes/footer.php');
+?>
