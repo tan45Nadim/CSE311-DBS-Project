5a1c778f7a73e192b481edf7eb02141b2cea76cc
Tanjir Ahmed Nadim <106719777+tan45Nadim@users.noreply.github.com>
2023-05-02 02:34:26 +0600

    Add files via upload

--- versions/v_01/includes/header.php
+++ versions/v_01/includes/header.php
@@ -8,6 +8,13 @@
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
+  <!-- For Button -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+
+
   <title>PatientQ</title>
 </head>
 <body>
